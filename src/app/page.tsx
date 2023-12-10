8447c47a26a5e0204ecc2bfeb524b355ce56cdf9
Mohamed Gibreel <m.adilgibreel@gmail.com>
2023-12-11 00:24:08 +0400

    feat: Fetch & show products in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import ProductReel from "@/components/ProductReel";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { ArrowDownLeftFromCircleIcon, CheckCircle, Leaf } from "lucide-react";
 import Link from "next/link";
@@ -7,7 +8,8 @@ const perks = [
   {
     name: "Instant Delivery",
     Icon: ArrowDownLeftFromCircleIcon,
-    description: "Get your assets delivered to your email in seconds and download them right away",
+    description:
+      "Get your assets delivered to your email in seconds and download them right away",
   },
   {
     name: "Guranteed Quality",
@@ -29,11 +31,12 @@ export default function Home() {
       <MaxWidthWrapper>
         <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
           <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-            Your marketplace for high-quality <span className="text-blue-600">digital assets</span>.
+            Your marketplace for high-quality{" "}
+            <span className="text-blue-600">digital assets</span>.
           </h1>
           <p className="mt-6 text-large max-w-prose text-muted-foreground">
-            Welcome to DigitalHippo. Ever asset on our platform is verified by our team to ensure
-            our highest quality standards.
+            Welcome to DigitalHippo. Ever asset on our platform is verified by
+            our team to ensure our highest quality standards.
           </p>
           <div className="flex flex-col sm:flex-row gap-4 mt-6">
             <Link href="/products" className={buttonVariants()}>
@@ -43,6 +46,11 @@ export default function Home() {
           </div>
         </div>
         {/* TODO: List products */}
+        <ProductReel
+          query={{ sort: "desc", limit: 4 }}
+          title="Brand new"
+          href="/products"
+        />
         {/* Testing */}
       </MaxWidthWrapper>
       <section className="border-t border-gray-200 bg-gray-50">
@@ -59,8 +67,12 @@ export default function Home() {
                   </div>
                 </div>
                 <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
-                  <h3 className="text-base font-medium text-gray-900">{name}</h3>
-                  <p className="mt-3 text-sm text-muted-foreground">{description}</p>
+                  <h3 className="text-base font-medium text-gray-900">
+                    {name}
+                  </h3>
+                  <p className="mt-3 text-sm text-muted-foreground">
+                    {description}
+                  </p>
                 </div>
               </div>
             ))}
---
+++ src/components/ImageSlider.tsx
@@ -0,0 +1,107 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import type SwiperType from "swiper";
+import { Pagination } from "swiper/modules";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+interface Props {
+  urls: string[];
+}
+
+const ImageSlider = ({ urls }: Props) => {
+  const [swiper, setSwiper] = useState<SwiperType | null>(null);
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+  const [slideConfig, setSlideConfig] = useState({
+    isBeginning: true,
+    isEnd: activeIndex === (urls.length ?? 0) - 1,
+  });
+
+  const activeStyles =
+    "active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square w-8 h-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300";
+
+  const inactiveStyles = "pointer-events-none opacity-0 text-gray-400";
+
+  useEffect(() => {
+    swiper?.on("slideChange", ({ activeIndex }) => {
+      setActiveIndex(activeIndex);
+      setSlideConfig({
+        isBeginning: activeIndex === 0,
+        isEnd: activeIndex === (urls.length ?? 0) - 1,
+      });
+    });
+  }, [swiper, urls]);
+
+  return (
+    <div className="group relative bg-zing-100 aspect-square overflow-hidden rounded-xl">
+      <div className="absolute z-10 inset-0 opacity-0 transition group-hover:opacity-100">
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            swiper?.slideNext();
+          }}
+          className={cn(activeStyles, "right-3 transition", {
+            [inactiveStyles]: slideConfig.isEnd,
+            "hover:bg-primary-300 text-primary-800 opactiy-100":
+              !slideConfig.isEnd,
+          })}
+        >
+          <ChevronRight
+            className="h-4 w-4 text-zinc-700"
+            aria-label="next image"
+          ></ChevronRight>
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            swiper?.slidePrev();
+          }}
+          className={cn(activeStyles, "left-3 transition", {
+            [inactiveStyles]: slideConfig.isBeginning,
+            "hover:bg-primary-300 text-primary-800 opactiy-100":
+              !slideConfig.isBeginning,
+          })}
+        >
+          <ChevronLeft
+            className="h-4 w-4 text-zinc-700"
+            aria-label="prev image"
+          ></ChevronLeft>
+        </button>
+      </div>
+      <Swiper
+        pagination={{
+          renderBullet: (_, className) => {
+            return `<span class="rounded-full transition ${className}"></span>`;
+          },
+        }}
+        onSwiper={setSwiper}
+        spaceBetween={50}
+        slidesPerView={1}
+        modules={[Pagination]}
+        className="h-full w-full"
+      >
+        {urls.map((url, index) => {
+          return (
+            <SwiperSlide
+              key={`${url}-${index}`}
+              className="-z-10 h-full w-full"
+            >
+              <Image
+                fill
+                loading="eager"
+                src={url}
+                alt="product-image"
+                className="-z-10 h-full w-full object-cover object-center"
+              />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
+};
+
+export default ImageSlider;
