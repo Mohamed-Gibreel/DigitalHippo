1e599ac052b01ab351a941002514190a8b050236
Mohamed Gibreel <m.adilgibreel@gmail.com>
2023-12-20 17:52:16 +0400

    feat: add cart drawer

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -14,9 +14,18 @@ import { formatPrice } from "@/lib/utils";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
 import Image from "next/image";
+import { useCart } from "@/hooks/use-cart";
+import CartItem from "./CartItem";
+import { ProductFiles } from "@/collections/ProductFile";
+import { ScrollArea } from "./ui/scroll-area";
 
 const Cart = () => {
-  const itemCount = 0;
+  const { items } = useCart();
+  const itemCount = items.length;
+  const cartTotal = items.reduce((total, { product }) => {
+    return total + product.price;
+  }, 0);
+  const fee = 1;
   return (
     <Sheet>
       <SheetTrigger className="group -m-2 flex items-center p-2">
@@ -25,18 +34,22 @@ const Cart = () => {
           className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
         />
         <span className="ml-2 text-sm font-medium text-gray-700 group:hover:text-gray-800">
-          0
+          {itemCount}
         </span>
       </SheetTrigger>
-      <SheetContent className="flex w-full flex-col pre-0 sm:max-w-lg">
+      <SheetContent className="flex w-full h-full flex-col pre-0 sm:max-w-lg">
         <SheetHeader className="space-y-2.5 pr-6">
-          <SheetTitle>Cart (0)</SheetTitle>
+          <SheetTitle>Cart ({itemCount})</SheetTitle>
         </SheetHeader>
         {itemCount > 0 ? (
           <>
-            <div className="flex w-full flex-col pr-6">
+            <div className="flex w-full flex-col pr-6 overflow-auto flex-1">
               {/* TODO: Cart Logic */}
-              Cart Items
+              <ScrollArea className="h-auto">
+                {items.map(({ product }) => (
+                  <CartItem key={product.id} product={product} />
+                ))}
+              </ScrollArea>
             </div>
             <div className="space-y-4 pr-6">
               <Separator />
@@ -47,11 +60,11 @@ const Cart = () => {
                 </div>
                 <div className="flex ">
                   <span className="flex-1">Transaction Fee</span>
-                  <span>{formatPrice(1)} $</span>
+                  <span>{formatPrice(fee)} $</span>
                 </div>
                 <div className="flex ">
                   <span className="flex-1">Total</span>
-                  <span>{formatPrice(1)} $</span>
+                  <span>{formatPrice(cartTotal + fee)} $</span>
                 </div>
                 <SheetFooter>
                   <SheetTrigger asChild>
@@ -70,15 +83,8 @@ const Cart = () => {
           </>
         ) : (
           <div className="flex h-full flex-col items-center justify-center space-y-1">
-            <div
-              aria-hidden="true"
-              className="relative mb-4 h-60 w-60 text-muted-foreground"
-            >
-              <Image
-                src={"/hippo-empty-cart.png"}
-                alt="Empty Cart Image"
-                fill
-              />
+            <div aria-hidden="true" className="relative mb-4 h-60 w-60 text-muted-foreground">
+              <Image src={"/hippo-empty-cart.png"} alt="Empty Cart Image" fill />
             </div>
             <div className="text-xl font-semibold">Your Cart is empty</div>
             <SheetTrigger asChild>
